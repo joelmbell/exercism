5c17a42c291e1944499b248443a1648db3996ed1
Joel Bell <joel.m.bell@gmail.com>
2017-10-22 12:48:00 -0700

    Clojure word-count version 2

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -5,22 +5,20 @@
   (str/split phrase #" "))
 
 (defn is-punctuation? [char]
-  (and
-    (not (Character/isLetterOrDigit char))
-    (not (Character/isWhitespace char))))
+  (and (not (Character/isLetterOrDigit char))
+       (not (Character/isWhitespace char))))
 
 (defn filter-punctuation [phrase]
-  (apply str (filter (fn [x] (not (is-punctuation? x)))
-                     phrase)))
+  (apply str (remove is-punctuation? phrase)))
 
 (defn count-occurances [element list]
-  (count (filter (fn [x] (= x element)) list)))
+  (count
+    (filter #(= % element)
+            list)))
 
 (defn word-count [phrase]
   (let [cleaned     (filter-punctuation (str/lower-case phrase))
-        word-list   (filter (fn [x] (not (str/blank? x)))
-                            (split-words cleaned))
-        word-count  (map (fn [x] (count-occurances x word-list))
+        word-list   (remove str/blank? (split-words cleaned))
+        word-count  (map #(count-occurances % word-list)
                          word-list)]
     (zipmap word-list word-count)))
-
