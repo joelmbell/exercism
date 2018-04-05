d31282b658a63b62ccbbed1719f25ed95b6dcf6d
Joel Bell <joelbell@barrick.com>
2018-04-05 08:56:23 -0700

    Go Space Age Version 2

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,33 +1,28 @@
 // Package space contains a utility function for calculating age based on what planet you are on
 package space
 
-type Planet = string
+type Planet string
+
+const earthYear = 365.25 * 24 * 60 * 60
+
+var orbitalPeriods = map[string]float64{
+	"Earth":   earthYear,
+	"Mercury": earthYear * 0.2408467,
+	"Venus":   earthYear * 0.61519726,
+	"Mars":    earthYear * 1.8808158,
+	"Jupiter": earthYear * 11.862615,
+	"Saturn":  earthYear * 29.447498,
+	"Uranus":  earthYear * 84.016846,
+	"Neptune": earthYear * 164.79132,
+}
+
+func (p *Planet) getAgeInYears(seconds float64) float64 {
+	return seconds / orbitalPeriods[string(*p)]
+}
 
 // function Age returns the age you are in years
 // based on the number of seconds you've lived command
 // the planet you live on.
 func Age(seconds float64, plant Planet) float64 {
-
-	earthAge := seconds / 60 / 60 / 24 / 365.25
-
-	switch plant {
-	case "Earth":
-		return earthAge
-	case "Mercury":
-		return earthAge / 0.2408467
-	case "Venus":
-		return earthAge / 0.61519726
-	case "Mars":
-		return earthAge / 1.8808158
-	case "Jupiter":
-		return earthAge / 11.862615
-	case "Saturn":
-		return earthAge / 29.447498
-	case "Uranus":
-		return earthAge / 84.016846
-	case "Neptune":
-		return earthAge / 164.79132
-	default:
-		return 0.0
-	}
+	return plant.getAgeInYears(seconds)
 }
