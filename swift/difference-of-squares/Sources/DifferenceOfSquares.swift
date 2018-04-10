2a0da9f94b5be15dc2734f6c5c5849d940b34906
Joel Bell <joelbell@barrick.com>
2018-04-10 12:05:45 -0700

    Swift difference of squares v3
    Solves potential issue with below 0 input

--- swift/difference-of-squares/Sources/DifferenceOfSquares.swift
+++ swift/difference-of-squares/Sources/DifferenceOfSquares.swift
@@ -1,5 +1,7 @@
 //Solution goes in Sources
 
+import Foundation
+
 extension Sequence where Element: Numeric {
     func sum() -> Element {
         return reduce(0, +)
@@ -12,20 +14,19 @@ extension Sequence where Element: Numeric {
 
 class Squares {
 
-    let inputRange: CountableClosedRange<Int>
+    let inputRange: CountableClosedRange<UInt>
 
-    init(_ num: Int) {
-        inputRange = 1...num
+    init(_ num: UInt) {
+        inputRange = 0...num
     }
 
     var squareOfSums: Int {
         let sum = inputRange.sum()
-        return sum * sum
+        return Int(sum * sum)
     }
 
     var sumOfSquares: Int {
-        let squares = inputRange.square()
-        return squares.sum()
+        return Int(inputRange.square().sum())
     }
 
     var differenceOfSquares: Int {
