1741430eadaf0400f47d4388e31f7e8c8112c08c
Joel Bell <joelbell@barrick.com>
2018-04-10 10:01:46 -0700

    Go hamming v2

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,25 +1,15 @@
 // Package hamming exposes a function to calculate hamming distances
 package hamming
 
-import (
-	"errors"
-)
-
-var invalidStrandsError = errors.New("Strands are not equal size")
+import "errors"
 
 // Distance calculates the hamming distance between two DNA strands
 func Distance(a, b string) (int, error) {
 
-	max := max(len(a), len(b))
-	min := min(len(a), len(b))
-
-	// Verify strings are the same length
-	diff := max - min
-	if diff > 0 {
-		return diff * -1, invalidStrandsError
+	if len(a)-len(b) != 0 {
+		return -1, errors.New("Strands are not equal size")
 	}
 
-	// Calculate Distance
 	distance := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
@@ -27,22 +17,4 @@ func Distance(a, b string) (int, error) {
 		}
 	}
 	return distance, nil
-}
-
-// min returns the smaller value of the two inputs.
-// Math.Min is only for floating point, this implementation is for ints
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-// max returns the larger value of the two inputs.
-// Math.Max is only for floating point, this implementation is for ints
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
 }
