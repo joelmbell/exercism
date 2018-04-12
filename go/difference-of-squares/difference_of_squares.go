9f0327309d8de51de9347584ac7093b9741a01c8
Joel Bell <joelbell@barrick.com>
2018-04-11 20:56:03 -0700

    go diff of squares v1

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,29 @@
+// Package diffsquares diffs some squares
+package diffsquares
+
+// SumOfSquares sums all the values and squares them
+func SquareOfSums(v int) (sum int) {
+	for i := 1; i <= v; i++ {
+		sum += i
+	}
+	sum *= sum
+	return
+}
+
+// SumOfSquares squares all the values and sums them
+func SumOfSquares(v int) (result int) {
+	var squares = []int{}
+	for i := 1; i <= v; i++ {
+		squares = append(squares, i*i)
+	}
+
+	for k := 0; k < len(squares); k++ {
+		result += squares[k]
+	}
+	return
+}
+
+// Difference returns the SquareOfSums - SumOfSquares
+func Difference(v int) int {
+	return SquareOfSums(v) - SumOfSquares(v)
+}
