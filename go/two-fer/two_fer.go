8784f0e8aa2563aee7dfe14bba3cb7f6dc563e00
Joel Bell <joelbell@barrick.com>
2018-08-25 13:31:29 -0700

    Updating two-fer solution
    Updating go two-fer solution

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,12 +1,10 @@
 // Package twofer exposes a function for sharing with someone.
 package twofer
 
-import "fmt"
-
 // ShareWith allows you to share something with someone else.
 func ShareWith(s string) string {
 	if s == "" {
 		s = "you"
 	}
-	return fmt.Sprintf("One for %s, one for me.", s)
+	return "One for " + s + ", one for me."
 }
