2328c42a4ddc2473888e4ff7a83d9267ca9c9411
Joel Bell <joelbell@barrick.com>
2018-08-29 10:25:45 -0700

    Swift Accumulate V2

--- swift/accumulate/Sources/Accumulate.swift
+++ swift/accumulate/Sources/Accumulate.swift
@@ -1,24 +1,8 @@
 //Solution goes in Sources
 
 extension Sequence {
-    func accumulate(_ accumulator: (Element) -> Element) -> [Self.Element] {
-
-        // Why Can't I just use map??
-//        return self.map { accumulator($0) }
-
-        var result: [Self.Element] = []
-        for item in self {
-            result.append(accumulator(item))
-        }
-        return result
-    }
-
-    func accumulate(_ accumulator: (Element) -> [Element]) -> [[Self.Element]] {
-
-        // Why Can't I just use map??
-//        return self.map { accumulator($0) }
-
-        var result: [[Self.Element]] = []
+    func accumulate<T>(_ accumulator: (Element) -> T) -> [T] {
+        var result: [T] = []
         for item in self {
             result.append(accumulator(item))
         }
