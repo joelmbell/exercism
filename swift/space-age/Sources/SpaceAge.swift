1bb500ff2d5b6e2c08c37cc1559656a9ae918ec4
Joel Bell <joelbell@barrick.com>
2018-09-02 18:47:06 -0700

    Space Age V2

--- swift/space-age/Sources/SpaceAge.swift
+++ swift/space-age/Sources/SpaceAge.swift
@@ -2,47 +2,64 @@
 
 struct SpaceAge {
 
-    let seconds: Double
+    enum Plant: Double {
+        case earth = 1
+        case mercury = 0.2408467
+        case venus = 0.61519726
+        case mars = 1.8808158
+        case jupiter = 11.862615
+        case saturn = 29.447498
+        case uranus = 84.016846
+        case neptune = 164.49132
 
-    private let earthYearSeconds = 31557600.0
+        var earthYearSeconds: Double {
+            return 31557600.0
+        }
+
+        var orbitalPeriod: Double {
+            return earthYearSeconds * self.rawValue
+        }
+
+        func years(from seconds: Double) -> Double {
+            return seconds / self.orbitalPeriod
+        }
+    }
+
+    let seconds: Double
 
     init(_ seconds: Double) {
         self.seconds = seconds
     }
 
     var onEarth: Double {
-        return seconds / earthYearSeconds
+        return Plant.earth.years(from: seconds)
     }
 
     var onMercury: Double {
-        return seconds / orbitalPeriod(relativeTo: 0.2408467)
+        return Plant.mercury.years(from: seconds)
     }
 
     var onVenus: Double {
-        return seconds / orbitalPeriod(relativeTo: 0.61519726)
+        return Plant.venus.years(from: seconds)
     }
 
     var onMars: Double {
-        return seconds / orbitalPeriod(relativeTo: 1.8808158)
+        return Plant.mars.years(from: seconds)
     }
 
     var onJupiter: Double {
-        return seconds / orbitalPeriod(relativeTo: 11.862615)
+        return Plant.jupiter.years(from: seconds)
     }
 
     var onSaturn: Double {
-        return seconds / orbitalPeriod(relativeTo: 29.447498)
+        return Plant.saturn.years(from: seconds)
     }
 
     var onUranus: Double {
-        return seconds / orbitalPeriod(relativeTo: 84.016846)
+        return Plant.uranus.years(from: seconds)
     }
 
     var onNeptune: Double {
-        return seconds / orbitalPeriod(relativeTo: 164.79132)
-    }
-
-    private func orbitalPeriod(relativeTo relativeEarthYears: Double) -> Double {
-        return earthYearSeconds * relativeEarthYears
+        return Plant.neptune.years(from: seconds)
     }
 }
